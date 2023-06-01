0b30e368dc1d0c31f8bec41ae238c104df40457d
Aliyah Indra <aliyah.indra@gmail.com>
2023-06-01 10:49:20 -0700

    updated server port

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const app = require('./index');
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`Server available at http://localhost:${PORT}`);
